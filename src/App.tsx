d24e3f576cbcf186cd5da497899919fd410b97d5
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2021-10-24 23:03:29 +0200

    Iterate on PoC

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { Add, Check, Circle, Close, Remove } from "@mui/icons-material";
-import {
-  CheckIcon,
-  DotFillIcon,
-  GitPullRequestIcon,
-  XIcon,
-} from "@primer/octicons-react";
-import { formatRelative } from "date-fns";
+import { CheckIcon, DotFillIcon, GitPullRequestIcon, XIcon } from "@primer/octicons-react";
 import { useEffect, useState } from "react";
 import { api } from "./api";
 import "./App.css";
 import { Spinner } from "./Spinner";
+import megaquery from "./megaquery.graphql?raw";
 
 function App() {
   const [data, setData] = useState<any>(null);
@@ -17,82 +11,28 @@ function App() {
   useEffect(() => {
     (async () => {
       setData(null);
-      const result = await api(`
-{
-  viewer {
-    name
-repositories(last: 100) {
-nodes {
-id
-name
-owner {
-login
-}
-pullRequests(first: 10, states: OPEN) {
-nodes {
-id
-title
-createdAt
-mergeable
-number
-isDraft
-url
-viewerDidAuthor
-labels(first: 5) {
-    nodes {
-        name
-    }
-}
-potentialMergeCommit {
-status {state}
-              statusCheckRollup {
-              state
-                }
-}
-commits(last: 1){
-          nodes{
-            commit{
-              status {
-                state
-              }
-              statusCheckRollup {
-              state
-                }
-            }
-          }
-        }
-assignees(first: 5) {
-    nodes {
-        id
-        avatarUrl
-        login
-    }
-}
-author {
-    login
-    avatarUrl
-}
-reviewDecision
-}
-}
-}
-}
-}
-}
-    `);
-      const obj = await result.json();
       const owners: { [owner: string]: any[] } = {};
-      const repos = obj.data.viewer.repositories.nodes.filter(
-        (repo: any) => repo.pullRequests.nodes.length > 0
-      );
-      repos.forEach((repo: any) => {
-        const ownerName = repo.owner.login;
-        owners[ownerName] = owners[ownerName] || [];
-        owners[ownerName] = owners[ownerName].concat(repo.pullRequests.nodes);
-        for (const pr of repo.pullRequests.nodes) {
-          pr.repo = repo;
+      let after: string | null = null;
+
+      while (true) {
+        let result: any = await api(megaquery, { after });
+        let obj = await result.json();
+        console.log(obj.data.viewer.repositories.nodes);
+        const repos = obj.data.viewer.repositories.nodes.filter((repo: any) => repo.pullRequests.nodes.length > 0);
+        console.log(repos);
+        repos.forEach((repo: any) => {
+          const ownerName = repo.owner.login;
+          owners[ownerName] = owners[ownerName] || [];
+          owners[ownerName] = owners[ownerName].concat(repo.pullRequests.nodes);
+          for (const pr of repo.pullRequests.nodes) {
+            pr.repo = repo;
+          }
+        });
+        if (!obj.data.viewer.repositories.pageInfo.hasNextPage) {
+          break;
         }
-      });
+        after = obj.data.viewer.repositories.pageInfo.endCursor;
+      }
       const newData = Object.entries(owners)
         .map(([name, prs]) => ({ name, prs }))
         .sort((a, b) => (a.name > b.name ? 1 : -1));
@@ -105,15 +45,11 @@ reviewDecision
 
   const [showWIPs, setShowWIPs] = useState(false);
 
-  const [selectedOwner, _setSelectedOwner] = useState(
-    localStorage.getItem("selectedOwner")
-  );
+  const [selectedOwner, _setSelectedOwner] = useState(localStorage.getItem("selectedOwner"));
   const setSelectedOwner = (owner: string) => {
     _setSelectedOwner(owner);
     localStorage.setItem("selectedOwner", owner);
   };
-
-  const selectedRepos = data?.find(({ name }) => name === selectedOwner);
 
   if (!data) {
     return (
@@ -126,61 +62,54 @@ reviewDecision
     );
   }
 
+  let filteredData = data.map((obj) => ({ ...obj }));
+  filteredData.forEach((obj) => {
+    obj.prs = obj.prs.filter((pr) => {
+      const isWip = pr.isDraft || pr.title.trim().toLowerCase().replaceAll(/\[|\]/g, "").startsWith("wip");
+      return !(isWip && !showWIPs);
+    });
+  });
+  filteredData = filteredData.filter((obj) => obj.prs.length > 0);
+  const selectedPrs = filteredData.find((obj) => obj.name === selectedOwner);
+
   return (
     <div className="container px-3 mx-auto sm">
       <div className="mt-3">
-        {data &&
-          data.map(({ name }) => (
-            <button
-              className={`py-1 px-5 border rounded divide-opacity-0 mr-3 ${
-                name === selectedOwner ? "bg-blue-200" : ""
-              }`}
-              key={name}
-              onClick={(e) => {
-                e.preventDefault();
-                setSelectedOwner(name);
-              }}
-            >
-              {name}
-            </button>
-          ))}
+        {filteredData.map(({ name }) => (
+          <button
+            className={`py-1 px-5 border rounded divide-opacity-0 mr-3 ${name === selectedOwner ? "bg-blue-200" : ""}`}
+            key={name}
+            onClick={(e) => {
+              e.preventDefault();
+              setSelectedOwner(name);
+            }}
+          >
+            {name}
+          </button>
+        ))}
       </div>
 
       <label className="flex items-center my-3">
-        <input
-          type="checkbox"
-          checked={showWIPs}
-          onChange={(e) => setShowWIPs(e.target.checked)}
-          className="mr-3"
-        />
+        <input type="checkbox" checked={showWIPs} onChange={(e) => setShowWIPs(e.target.checked)} className="mr-3" />
         <div>Show WIPs</div>
       </label>
 
       <div className="divide-y">
-        {selectedRepos?.prs.map((pr: any) => {
+        {selectedPrs?.prs.map((pr: any) => {
           const needsRebase = pr.mergeable === "CONFLICTING";
           const isAuthor = pr.viewerDidAuthor;
-          const isWip =
-            pr.isDraft ||
-            pr.title
-              .trim()
-              .toLowerCase()
-              .replaceAll(/\[|\]/g, "")
-              .startsWith("wip");
+          const isWip = pr.isDraft || pr.title.trim().toLowerCase().replaceAll(/\[|\]/g, "").startsWith("wip");
 
           if (isWip && !showWIPs) {
             return null;
           }
 
           const needsReview = pr.reviewDecision === "REVIEW_REQUIRED" && !isWip;
-          const needsAssignee =
-            !pr.assignees || pr.assignees.nodes.length === 0;
+          const needsAssignee = !pr.assignees || pr.assignees.nodes.length === 0;
           const youAreAssigned =
             pr.assignees &&
             pr.assignees.nodes.length > 0 &&
-            pr.assignees.nodes.findIndex(
-              (assignee) => assignee.login === "sigvef"
-            ) !== -1;
+            pr.assignees.nodes.findIndex((assignee) => assignee.login === "sigvef") !== -1;
           let shouldHighlight = false;
           if (isAuthor && needsRebase) {
             shouldHighlight = true;
@@ -198,11 +127,7 @@ reviewDecision
             shouldHighlight = false;
           }
 
-          if (
-            pr.labels?.nodes.find(
-              (label: any) => label.name.toLowerCase() === "skip colorization"
-            )
-          ) {
+          if (pr.labels?.nodes.find((label: any) => label.name.toLowerCase() === "skip colorization")) {
             shouldHighlight = false;
           }
 
@@ -221,41 +146,29 @@ reviewDecision
           return (
             <div
               key={pr.id}
-              className={`h-16 px-4 flex ${
-                isWip ? "bg-gray-200 font-thin" : ""
-              } ${shouldHighlight ? "bg-yellow-100" : ""}`}
+              className={`h-16 px-4 flex ${isWip ? "bg-gray-200 font-thin" : ""} ${
+                shouldHighlight ? "bg-yellow-100" : ""
+              }`}
             >
               <div className="self-center w-24 font-thin text-right whitespace-nowrap overflow-hidden overflow-ellipsis">
                 {pr.repo.name}
               </div>
               <div className="flex items-center self-center justify-center flex-shrink-0 w-12 ml-3 mr-3">
-                {pr.commits.nodes[0].commit.statusCheckRollup?.state ===
-                  "PENDING" && (
-                  <DotFillIcon
-                    className={!isWip ? "text-yellow-500" : "text-gray-500"}
-                  />
+                {pr.commits.nodes[0].commit.statusCheckRollup?.state === "PENDING" && (
+                  <DotFillIcon className={!isWip ? "text-yellow-500" : "text-gray-500"} />
                 )}
-                {pr.commits.nodes[0].commit.statusCheckRollup?.state ===
-                  "FAILURE" && (
-                  <XIcon
-                    className={!isWip ? "text-red-500" : "text-gray-500"}
-                  />
+                {pr.commits.nodes[0].commit.statusCheckRollup?.state === "FAILURE" && (
+                  <XIcon className={!isWip ? "text-red-500" : "text-gray-500"} />
                 )}
-                {pr.commits.nodes[0].commit.statusCheckRollup?.state ===
-                  "SUCCESS" && (
-                  <CheckIcon
-                    className={!isWip ? "text-green-500" : "text-gray-500"}
-                  />
+                {pr.commits.nodes[0].commit.statusCheckRollup?.state === "SUCCESS" && (
+                  <CheckIcon className={!isWip ? "text-green-500" : "text-gray-500"} />
                 )}
                 {!pr.commits.nodes[0].commit.statusCheckRollup?.state && (
                   <div className="font-thin text-gray-500">—</div>
                 )}
               </div>
               <div className="mr-5 w-6 h-6 flex-shrink-0 self-center">
-                <img
-                  src={pr.author.avatarUrl}
-                  className="w-6 h-6 rounded-full shadow"
-                />
+                <img src={pr.author.avatarUrl} className="w-6 h-6 rounded-full shadow" />
               </div>
               <div className="flex items-center ml-2">
                 <div>
@@ -287,9 +200,7 @@ reviewDecision
                 {pr.assignees.nodes.length === 0 &&
                   needsReview &&
                   (isAuthor ? (
-                    <div className="rounded-full border-2 border-opacity-0 px-5 py-1 text-gray-500">
-                      Needs review
-                    </div>
+                    <div className="rounded-full border-2 border-opacity-0 px-5 py-1 text-gray-500">Needs review</div>
                   ) : (
                     <div className="rounded-full border-2 px-5 py-1 border-yellow-700 text-yellow-700">
                       Needs review
@@ -297,10 +208,7 @@ reviewDecision
                   ))}
                 {pr.assignees.nodes.map((assignee: any) => (
                   <div key={assignee.id} className="ml-3 w-8 h-8 flex-shrink-0">
-                    <img
-                      src={assignee.avatarUrl}
-                      className="w-8 h-8 rounded-full shadow"
-                    />
+                    <img src={assignee.avatarUrl} className="w-8 h-8 rounded-full shadow" />
                   </div>
                 ))}
               </div>
--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,11 @@
 const token = import.meta.env.VITE_GH_TOKEN;
-export function api(query: any) {
+export function api(query: any, variables: any) {
   const headers = new Headers();
   headers.set("Content-Type", "application/json");
   headers.set("Authorization", `bearer ${token}`);
   return fetch("https://api.github.com/graphql", {
     method: "POST",
-    body: JSON.stringify({ query }),
+    body: JSON.stringify({ query, variables }),
     headers,
   });
 }
