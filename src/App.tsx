62a7d932e4e41c301aa393896b4a506fda5d0007
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2021-10-24 20:20:11 +0200

    Iterate on PoC

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Circle } from "@mui/icons-material";
+import { Check, Circle, Close, Remove } from "@mui/icons-material";
+import { colors } from "@mui/material";
+import { formatRelative } from "date-fns";
 import { useEffect, useState } from "react";
 import "./App.css";
 
@@ -24,36 +26,33 @@ function App() {
 {
   viewer {
     name
-repositories(first: 100) {
+repositories(last: 100) {
 nodes {
 id
 name
 owner {
 login
 }
-pullRequests(first: 100, states: OPEN) {
+pullRequests(first: 10, states: OPEN) {
 nodes {
 id
 title
 createdAt
 mergeable
 number
+isDraft
 url
 viewerDidAuthor
+labels(first: 5) {
+    nodes {
+        name
+    }
+}
 commits(last: 1){
           nodes{
             commit{
-              commitUrl
-              oid
               status {
                 state
-                
-                contexts {
-                  state
-                  targetUrl
-                  description
-                  context
-                }
               }
             }
           }
@@ -69,11 +68,6 @@ author {
     login
 }
 reviewDecision
-reviewRequests {
-    nodes {
-id
-    }
-}
 }
 }
 }
@@ -82,103 +76,187 @@ id
 }
     `);
       const obj = await result.json();
+      const owners: { [owner: string]: any[] } = {};
       const repos = obj.data.viewer.repositories.nodes.filter(
         (repo: any) => repo.pullRequests.nodes.length > 0
       );
       repos.forEach((repo: any) => {
-        repo.pullRequests.nodes.sort((a: any, b: any) =>
-          a.createdAt > b.createdAt ? a : b
-        );
+        const ownerName = repo.owner.login;
+        owners[ownerName] = owners[ownerName] || [];
+        owners[ownerName] = owners[ownerName].concat(repo.pullRequests.nodes);
+        for (const pr of repo.pullRequests.nodes) {
+          pr.repo = repo;
+        }
       });
-      repos.sort((a: any, b: any) =>
-        a.pullRequests.nodes[0].createdAt > b.pullRequests.nodes[0].createdAt
-          ? a
-          : b
-      );
-      setData(repos);
+      const newData = Object.entries(owners)
+        .map(([name, prs]) => ({ name, prs }))
+        .sort((a, b) => (a.name > b.name ? 1 : -1));
+      for (const obj of newData) {
+        obj.prs.sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1));
+      }
+      setData(newData);
     })();
   }, []);
 
+  const [showWIPs, setShowWIPs] = useState(false);
+
+  const [selectedOwner, _setSelectedOwner] = useState(
+    localStorage.getItem("selectedOwner")
+  );
+  const setSelectedOwner = (owner: string) => {
+    _setSelectedOwner(owner);
+    localStorage.setItem("selectedOwner", owner);
+  };
+
+  const selectedRepos = data?.find(({ name }) => name === selectedOwner);
+
   return (
     <div className="container sm mx-auto px-3">
-      {data &&
-        data.map((repo: any) => {
-          return (
-            <div key={repo.id} className="mb-5">
-              <div className="font-bold p-3">
-                {repo.owner.login}/{repo.name}
-              </div>
-              {repo.pullRequests.nodes.map((pr: any) => {
-                const needsRebase = pr.mergeable === "CONFLICTING";
-                const isAuthor = pr.viewerDidAuthor;
-                const needsReview = pr.reviewDecision === "REVIEW_REQUIRED";
-                const needsAssignee =
-                  !pr.assignees || pr.assignees.nodes.length === 0;
-                const youAreAssigned =
-                  pr.assignees &&
-                  pr.assignees.nodes.length > 0 &&
-                  pr.assignees.nodes.findIndex(
-                    (assignee) => assignee.login === "sigvef"
-                  ) !== -1;
-                let shouldHighlight = false;
-                if (isAuthor && needsRebase) {
-                  shouldHighlight = true;
-                }
-                if (!isAuthor && needsReview) {
-                  shouldHighlight = true;
-                }
-                if (needsAssignee && !isAuthor) {
-                  shouldHighlight = true;
-                }
-                if (!needsAssignee && !youAreAssigned && !isAuthor) {
-                  shouldHighlight = false;
-                }
-                return (
-                  <div
-                    key={pr.id}
-                    className={`mb-3 p-3 rounded flex ${
-                      shouldHighlight ? "bg-yellow-100" : ""
-                    }`}
-                  >
-                    <div className="w-12">
-                      <Circle />
-                      {pr.commits.nodes[0].commit.status?.state}
-                    </div>
-                    <div>
-                      <div>
-                        <a
-                          href={pr.url}
-                          target="_blank"
-                          className="flex flex-row items-center"
-                        >
-                          <div>{pr.title}</div>
-                          <div className="ml-5 font-thin">#{pr.number}</div>
-                        </a>
-                      </div>
-                      <div>{pr.author?.login ?? "No author"}</div>
-                      <div>{needsRebase && "Needs rebase"}</div>
-                      {JSON.stringify(pr)}
-                    </div>
+      <div className="mt-3">
+        {data &&
+          data.map(({ name }) => (
+            <button
+              className={`py-1 px-5 border rounded divide-opacity-0 mr-3 ${
+                name === selectedOwner ? "bg-blue-200" : ""
+              }`}
+              key={name}
+              onClick={(e) => {
+                e.preventDefault();
+                setSelectedOwner(name);
+              }}
+            >
+              {name}
+            </button>
+          ))}
+      </div>
 
-                    <div>
-                      {pr.assignees.nodes.map((assignee: any) => (
-                        <div
-                          key={assignee.id}
-                          className="w-8 h-8 flex-shrink-0"
-                        >
-                          <img
-                            src={assignee.avatarUrl}
-                            className="w-8 h-8 rounded-full shadow"
-                          />
-                        </div>
-                      ))}
+      <label className="flex items-center my-3">
+        <input
+          type="checkbox"
+          checked={showWIPs}
+          onChange={(e) => setShowWIPs(e.target.checked)}
+          className="mr-3"
+        />
+        <div>Show WIPs</div>
+      </label>
+
+      <div className="divide-y">
+        {selectedRepos?.prs.map((pr: any) => {
+          const needsRebase = pr.mergeable === "CONFLICTING";
+          const isAuthor = pr.viewerDidAuthor;
+          const isWip =
+            pr.isDraft ||
+            pr.title
+              .trim()
+              .toLowerCase()
+              .replaceAll(/\[|\]/g, "")
+              .startsWith("wip");
+
+          if (isWip && !showWIPs) {
+            return null;
+          }
+
+          const needsReview = pr.reviewDecision === "REVIEW_REQUIRED" && !isWip;
+          const needsAssignee =
+            !pr.assignees || pr.assignees.nodes.length === 0;
+          const youAreAssigned =
+            pr.assignees &&
+            pr.assignees.nodes.length > 0 &&
+            pr.assignees.nodes.findIndex(
+              (assignee) => assignee.login === "sigvef"
+            ) !== -1;
+          let shouldHighlight = false;
+          if (isAuthor && needsRebase) {
+            shouldHighlight = true;
+          }
+          if (!isAuthor && needsReview) {
+            shouldHighlight = true;
+          }
+          if (needsAssignee && !isAuthor) {
+            shouldHighlight = true;
+          }
+          if (!needsAssignee && !youAreAssigned && !isAuthor) {
+            shouldHighlight = false;
+          }
+          if (isWip && !isAuthor) {
+            shouldHighlight = false;
+          }
+
+          if (
+            pr.labels?.nodes.find(
+              (label: any) => label.name.toLowerCase() === "skip colorization"
+            )
+          ) {
+            shouldHighlight = false;
+          }
+
+          const needs = [];
+          if (needsRebase) {
+            needs.push("rebase");
+          }
+          if (needsReview) {
+            needs.push("review");
+          }
+          return (
+            <div
+              key={pr.id}
+              className={`p-3 flex ${shouldHighlight ? "bg-yellow-100" : ""}`}
+            >
+              <div className="w-24 text-right font-thin self-center">
+                {pr.repo.name}
+              </div>
+              <div className="w-12 ml-3 mr-3 flex-shrink-0 self-center justify-center flex items-center">
+                {pr.commits.nodes[0].commit.status?.state === "PENDING" && (
+                  <Circle className="text-yellow-500" />
+                )}
+                {pr.commits.nodes[0].commit.status?.state === "FAILURE" && (
+                  <Close className="text-red-500" />
+                )}
+                {pr.commits.nodes[0].commit.status?.state === "SUCCESS" && (
+                  <Check className="text-green-500" />
+                )}
+              </div>
+              <div>
+                <div>
+                  <a href={pr.url} target="_blank">
+                    <div className="flex items-center">
+                      {pr.title}
+                      <div className="flex items-center">
+                        {needs.map((need) => (
+                          <div
+                            className="ml-3 rounded-full border-2 border-yellow-700 px-3 py-1 text-yellow-800"
+                            id={need}
+                          >
+                            Needs {need}
+                          </div>
+                        ))}
+                      </div>
                     </div>
+                  </a>
+                </div>
+                <div className="text-gray-500">
+                  #{pr.number} opened{" "}
+                  {formatRelative(new Date(pr.createdAt), new Date())} by{" "}
+                  {pr.author?.login ?? "(unknown)"}
+                </div>
+              </div>
+
+              <div className="flex-1" />
+
+              <div>
+                {pr.assignees.nodes.map((assignee: any) => (
+                  <div key={assignee.id} className="w-8 h-8 flex-shrink-0">
+                    <img
+                      src={assignee.avatarUrl}
+                      className="w-8 h-8 rounded-full shadow"
+                    />
                   </div>
-                );
-              })}
+                ))}
+              </div>
             </div>
           );
         })}
+      </div>
     </div>
   );
 }
